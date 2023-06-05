2468cb65770c269b4938bdda666f6a160e2d1db3
Stephen-Ibe <saintkalson@gmail.com>
2023-06-05 11:24:11 +0100

    product page responisveness and code cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
 import { GetAllProductsApi } from "@/services";
 import Image from "next/image";
+import { BsCart4 } from "react-icons/bs";
 
 export default async function Home() {
   const products = await GetAllProductsApi();
 
   return (
     <main className="flex flex-col items-center justify-between min-h-screen">
-      <div className="container py-20 mx-auto">
+      <div className="container py-20 mx-auto space-y-20">
+        <div className="w-full flex items-center justify-between">
+          <div className="w-10/12">
+            <input
+              type="search"
+              className="w-full py-2 px-4 border-b-2 bg-transparent outline-none"
+              placeholder="Search for product..."
+            />
+          </div>
+          <div className="flex items-center justify-center gap-x-2">
+            <BsCart4 size={30} />
+            <p className="text-sm border h-8 w-8 flex items-center justify-center rounded-full">
+              4
+            </p>
+          </div>
+        </div>
         <div className="products--container">
           {products.map(({ category, id, image, price, title }) => (
             <div className="product h-[500px]" key={id}>
